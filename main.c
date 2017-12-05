89344248f06aae08f69eea27ee362441878733e1
unknown <Franz@LT470P-FRANZ.amd.e-technik.uni-rostock.de>
2017-12-05 14:39:49 +0100

    first add

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <msp430.h>
+
+
+void PWM(int freq, int pulse_width)
+{
+    P1DIR |= BIT2;                           // P1.2
+    P1SEL |= BIT2;                           // P1.2
+    CCR0 = 500000 / freq - 1;                // set CCR0 to the desired PWM frequency
+    CCTL1 = OUTMOD_7;                        // CCR1 toggle/set
+    CCR1 = pulse_width * 500000 / 1000000;   // set CCR1 to the desired PWM pulse width
+    TACTL = TASSEL_2 + MC_1 + ID_1;          // SMCLK, up-down mode, 500 kHz
+
+
+
+}
+
+int main(void)
+{
+
+    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+    BCSCTL1 = CALBC1_1MHZ;
+    DCOCTL  = CALDCO_1MHZ;
+
+    int freq = 130;                           // PWM frequency in Hz
+    int pulse_width = 60;                     // PWM pulse_width in microseconds
+
+    PWM(freq, pulse_width);
+
+    __bis_SR_register(LPM0_bits);             // Enter LPM0
+}
+
