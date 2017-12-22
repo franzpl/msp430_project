29888a7a864be3d49012de1de13fe7e2a578fd08
unknown <Franz@LT470P-FRANZ.amd.e-technik.uni-rostock.de>
2017-12-22 14:05:45 +0100

    add SPI init

--- main.c
+++ main.c
@@ -1,14 +1,41 @@
-#include <msp430.h>
+#include <msp430g2553.h>
 
 
 void PWM(int freq, int pulse_width)
 {
-    P1DIR |= BIT2;                           // P1.2
-    P1SEL |= BIT2;                           // P1.2
-    CCR0 = 500000 / freq - 1;                // set CCR0 to the desired PWM frequency
-    CCTL1 = OUTMOD_7;                        // CCR1 toggle/set
-    CCR1 = pulse_width * 0.5;             // set CCR1 to the desired PWM pulse width ( 500 kHz / 1e6)
-    TACTL = TASSEL_2 + MC_1 + ID_1;          // SMCLK, up-down mode, 500 kHz
+    P2DIR |= BIT1;                           // P2.1
+    P2SEL |= BIT1;                           // P2.1
+    TA1CCR0 = 500000 / freq - 1;                // set CCR0 to the desired PWM frequency
+    TA1CCTL1 = OUTMOD_7;                        // CCR1 toggle/set
+    TA1CCR1 = pulse_width * 0.5;             // set CCR1 to the desired PWM pulse width ( 500 kHz / 1e6)
+    TA1CTL = TASSEL_2 + MC_1 + ID_1;          // SMCLK, up-down mode, 500 kHz
+
+}
+
+void SPI_init() {
+
+
+        P1OUT |= BIT5; // 1.5 CS
+        P1DIR |= BIT5;
+        P1SEL = BIT2 | BIT4; // 1.1 UCA0SIMO, 1.4 UCA0CLK
+        P1SEL2 = BIT2 | BIT4;
+
+        UCA0CTL1 = UCSWRST;
+        UCA0CTL0 |= UCCKPH + UCMSB + UCMST + UCSYNC; // 3-pin, 8-bit SPI master
+        UCA0CTL1 |= UCSSEL_2; // SMCLK
+        UCA0BR0 |= 0x02; // /2
+        UCA0BR1 = 0; //
+        UCA0MCTL = 0; // No modulation
+        UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
+
+        P1OUT &= (~BIT5); // when CS is low, data is loaded into the serial register
+
+        while (!(IFG2 & UCA0TXIFG)); // USCI_A0 TX buffer ready?
+        UCA0TXBUF = 0x17; // Send 0xxx over SPI to Slave (0x17 = 200uA)
+
+        __delay_cycles(100000);
+        P1OUT |= (BIT5); // Transfer data to RDAC register when CS returns to high
+
 
 }
 
@@ -22,8 +49,9 @@ int main(void)
     const int freq = 130;                           // PWM frequency in Hz
     const int pulse_width = 60;                     // PWM pulse_width in microseconds
 
+    SPI_init();
+
     PWM(freq, pulse_width);
 
     __bis_SR_register(LPM0_bits);             // Enter LPM0
 }
-
