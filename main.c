694bc81ada88c13bf8d89607de00defc1c533c37
unknown <Franz@LT470P-FRANZ.amd.e-technik.uni-rostock.de>
2018-04-03 14:17:29 +0200

    build new header files

--- main.c
+++ main.c
@@ -1,43 +1,10 @@
 #include <msp430g2553.h>
+#include "SPI.h"
+#include "PWM.h"
+#include "CurrentSource.h"
+#include "NFC.h"
 
 
-void PWM(int freq, int pulse_width)
-{
-    P2DIR |= BIT1;                           // P2.1
-    P2SEL |= BIT1;                           // P2.1
-    TA1CCR0 = 500000 / freq - 1;                // set CCR0 to the desired PWM frequency
-    TA1CCTL1 = OUTMOD_7;                        // CCR1 toggle/set
-    TA1CCR1 = pulse_width * 0.5;             // set CCR1 to the desired PWM pulse width ( 500 kHz / 1e6)
-    TA1CTL = TASSEL_2 + MC_1 + ID_1;          // SMCLK, up-down mode, 500 kHz
-
-}
-
-void SPI_init() {
-
-
-        P1OUT |= BIT5; // 1.5 CS
-        P1DIR |= BIT5;
-        P1SEL = BIT2 | BIT4; // 1.2 UCA0SIMO, 1.4 UCA0CLK
-        P1SEL2 = BIT2 | BIT4;
-
-        UCA0CTL1 = UCSWRST;
-        UCA0CTL0 |= UCCKPH + UCMSB + UCMST + UCSYNC; // 3-pin, 8-bit SPI master
-        UCA0CTL1 |= UCSSEL_2; // SMCLK
-        UCA0BR0 |= 0x02; // /2
-        UCA0BR1 = 0; //
-        UCA0MCTL = 0; // No modulation
-        UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
-
-        P1OUT &= (~BIT5); // when CS is low, data is loaded into the serial register
-
-        while (!(IFG2 & UCA0TXIFG)); // USCI_A0 TX buffer ready?
-        UCA0TXBUF = 0x17; // Send 0xxx over SPI to Slave (0x17 = 200uA)
-
-        __delay_cycles(100000);
-        P1OUT |= (BIT5); // Transfer data to RDAC register when CS returns to high
-
-
-}
 
 int main(void)
 {
@@ -46,12 +13,20 @@ int main(void)
     BCSCTL1 = CALBC1_1MHZ;
     DCOCTL  = CALDCO_1MHZ;
 
-    const int freq = 130;                           // PWM frequency in Hz
-    const int pulse_width = 60;                     // PWM pulse_width in microseconds
 
-    SPI_init();
+    const int freq = 130;                           // PWM frequency in Hz
+    const int pulse_width = 60;                     // PWM pulse width in microseconds
+
+    SPISetup();
+    SetOutputCurrent(0x20);
+
 
     PWM(freq, pulse_width);
 
     __bis_SR_register(LPM0_bits);             // Enter LPM0
+
+    while(1)
+
+    {}
+
 }
